634ea5115727ff27ca4076d1be6b4162f9514288
Unnati Girase <unnaticodes07@gmail.com>
2025-09-04 20:40:38 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: "Grid Blazer Game",
+    description: "An engaging Python game built during Stanford Code in Place. Features custom graphics, animations, and a thrilling victory sequence, designed exclusively for the Code in Place environment.",
+    tags: ["Python"],
+    github: "https://github.com/unnati-078/Grid-Blazer",
+    demo: "https://codeinplace.stanford.edu/cip5/share/jtFEfc0Q0J1k0JnKry8T"
+  },
+  {
     title: "HomeBites — Community Homemade Food Platform",
     description: "MERN web app connecting people needing homemade food with nearby home cooks; pickup-based model and AI-powered recommendations.",
     tags: ["MERN", "AI", "Product Design"],
