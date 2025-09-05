b2944cb8b0e49921f4ff22f4bdd01a88c93c2e30
Unnati Girase <unnaticodes07@gmail.com>
2025-09-05 17:19:40 +0530

    Update skills.js

--- data/skills.js
+++ data/skills.js
@@ -1,5 +1,4 @@
 export const skills = [
-  "C++", "Python", "JavaScript", "TypeScript",
-  "React", "Next.js", "Node.js", "Express",
-  "Tailwind CSS", "MongoDB", "PostgreSQL", "Git/GitHub"
+  "C++", "Python", "JavaScript", "HTML & CSS",
+  "React", "Git/GitHub"
 ];
